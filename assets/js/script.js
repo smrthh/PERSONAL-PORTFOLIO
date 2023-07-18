9bc55ebc66b41a90d8aac1b416f49e58ba00ef0c
smrth.3 <126047448+smrthh@users.noreply.github.com>
2023-07-18 21:37:59 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const addEventOnElem = function (elem , type , callback){
+if (elem.length > 1) {
+    for(let i = 0 ; i < elem.length ; i++){
+        elem[i].addEventListener(type , callback)
+    }
+} else {
+    elem.addEventListener(type , callback)
+}
+}
+
+
+const navbar = document.querySelector("[data-navbar]")
+const navLinks = document.querySelectorAll("[data-nav-link]")
+const navToggler = document.querySelector("[data-nav-toggler]")
+
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active")
+  navToggler.classList.toggle("active")
+}
+
+addEventOnElem(navToggler , "click" , toggleNavbar);
+
+const closeNavbar = function () {
+    navbar.classList.remove("active")
+    navToggler.classList.remove("active")
+  }
+  
+  addEventOnElem(navLinks , "click" , closeNavbar);
+
+
+/*
+* -- HEADER ACTIVE
+*/
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll" , function(){
+    if(window.scrollY > 100){
+        header.classList.add("active")
+    }else{
+        header.classList.remove("active")
+    }
+})
+
+
+
+
